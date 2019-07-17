042d6594aeb59a2d809b9f84bc5def712aa62637
sumit.chatterjee@ottomate.com <sumit.chatterjee@ottomate.com>
2019-07-17 12:35:58 +0530

    test

--- app.js
+++ app.js
@@ -12,6 +12,10 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+app.get('/',(req, res) => {
+    res.send("Test");
+});
+
 db.connect((err) => {
   if (err) {
     console.log('Unable to connect to MySQL.')
